89d00318176b0df5e84e0e1305d7c3e19ea8efc0
Cory Fowler <syntaxc4@gmail.com>
2012-08-07 16:34:36 -0700

    changed code/ to source/

--- code/src/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('SERVICE_BUS_CONNECTION_STRING','Endpoint=https://[sb-namespace].servicebus.windows.net;SharedSecretIssuer=[sb-issuer];SharedSecretValue=[sb-access-key]');
-define('QUEUE_NAME','[sb-queue-name]');
-
-?>
